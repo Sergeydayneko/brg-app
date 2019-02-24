73d3fc98361b3d3a69c62c48a0bab1018c15f950
sdayneko <sergeydayneko@mail.ru>
2019-02-24 10:56:34 +0300

    97 done

---
+++ src/hoc/Aux.js
@@ -0,0 +1,11 @@
+/**
+ * High-order component для отрисовки бе дивов
+ *
+ * @author dayneko_si
+ * @since 23.02.2019
+ * @param props передаваемые свойства
+ * @returns {*} возвращает готовый объект для рендеринга
+ */
+const aux = (props) => props.children
+
+export default aux;
