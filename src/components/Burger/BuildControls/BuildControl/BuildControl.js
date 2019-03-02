309b690b3dbcc1909bb4a3fb43540699fc26d676
sergey <sergeydayneko@mail.ru>
2019-03-02 09:41:22 +0300

    134 done

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './BuildControl.css'
+
+/**
+ * Build control component class
+ * for adding ingredients
+ *
+ * @author dayneko_si
+ * @sinse 02.03.2019
+ * @param props
+ * @returns {*}
+ */
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Less}>Less</button>
+        <button className={classes.More}>More</button>
+    </div>
+);
+
+export default buildControl
